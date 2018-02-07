0e8c0b07eb5fdc347b472f3835d18391f335af60
mshroom <32199029+mshroom@users.noreply.github.com>
2018-02-07 15:35:55 +0200

    Add files via upload

---
+++ src/main/java/tikape/runko/database/DrinkkiDao.java
@@ -0,0 +1,70 @@
+
+package tikape.runko.database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import tikape.runko.domain.Drinkki;
+
+public class DrinkkiDao {
+    
+    private Database database;
+
+    public DrinkkiDao(Database database) {
+        this.database = database;
+    }
+
+    @Override
+    public Drinkki findOne(Integer key) throws SQLException {
+        Connection connection = database.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Drinkki WHERE id = ?");
+        stmt.setObject(1, key);
+
+        ResultSet rs = stmt.executeQuery();
+        boolean hasOne = rs.next();
+        if (!hasOne) {
+            return null;
+        }
+
+        Integer id = rs.getInt("id");
+        String nimi = rs.getString("nimi");
+
+        Drinkki d = new Drikki(id, nimi);
+
+        rs.close();
+        stmt.close();
+        connection.close();
+
+        return d;
+    }
+
+    @Override
+    public List<Drinkki> findAll() throws SQLException {
+
+        Connection connection = database.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Drinkki");
+
+        ResultSet rs = stmt.executeQuery();
+        List<Drinkki> drinkit = new ArrayList<>();
+        while (rs.next()) {
+            Integer id = rs.getInt("id");
+            String nimi = rs.getString("nimi");
+
+            drinkit.add(new Drinkki(id, nimi));
+        }
+
+        rs.close();
+        stmt.close();
+        connection.close();
+
+        return drinkit;
+    }
+
+    @Override
+    public void delete(Integer key) throws SQLException {
+        // ei toteutettu
+    }
+}
