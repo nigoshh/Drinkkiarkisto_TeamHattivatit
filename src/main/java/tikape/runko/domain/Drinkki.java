d216aeb46cc20ded93b649202c6a538e8882cfba
mshroom <32199029+mshroom@users.noreply.github.com>
2018-02-07 15:21:33 +0200

    Update Drinkki.java

--- src/main/java/tikape/runko/domain/Drinkki.java
+++ src/main/java/tikape/runko/domain/Drinkki.java
@@ -4,10 +4,14 @@ public class Drinkki {
 
     private Integer id;
     private String nimi;
+    private List<RaakaAine> raakaAineet;
+    private List<Kategoria> kategoriat;
 
     public Drinkki(Integer id, String nimi) {
         this.id = id;
         this.nimi = nimi;
+        this.raakaAineet = new ArrayList<>();
+        this.kategoriat = new ArrayList<>();
     }
 
     public Integer getId() {
@@ -24,6 +28,22 @@ public class Drinkki {
 
     public void setNimi(String nimi) {
         this.nimi = nimi;
+    }
+    
+    public List<RaakaAine> getRaakaAineet()  {
+        return this.raakaAineet;
+    }
+    
+    public List<RaakaAine> getKategoriat()  {
+        return this.kategoriat;
+    }
+    
+    public void lisaaRaakaAine(RaakaAine raakaAine) {
+        this.raakaAineet.add(raakaAine);
+    }
+    
+    public void lisaaKategoria(Kategoria kategoria) {
+        this.kategoriat.add(kategoria);
     }
 
 }
