45552af700ec88890cee754c8b147f3c5810ba14
mshroom <32199029+mshroom@users.noreply.github.com>
2018-02-14 14:34:15 +0200

    Add files via upload

--- src/main/java/tikape/runko/database/Dao.java
+++ src/main/java/tikape/runko/database/Dao.java
@@ -6,6 +6,8 @@ import java.util.*;
 public interface Dao<T, K> {
 
     T findOne(K key) throws SQLException;
+    
+    void save(T element) throws SQLException;
 
     List<T> findAll() throws SQLException;
 
