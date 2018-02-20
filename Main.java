c76c89440455686beba189443a1381ed2781633d
einoroine <36230647+einoroine@users.noreply.github.com>
2018-02-20 16:42:16 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -12,6 +12,11 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Database database = new Database("jdbc:sqlite:drinkit.db");
         database.init();
+        
+//        // herokun portille
+//        if (System.getenv("PORT") != null) {
+//            Spark.port(Integer.valueOf(System.getenv("PORT")));
+//        }
 
         DrinkkiDao drinkkiDao = new DrinkkiDao(database);
 
