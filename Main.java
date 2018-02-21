8566ac200558f88e9e8426e6b3d9f12e247c67a5
mshroom <32199029+mshroom@users.noreply.github.com>
2018-02-21 17:42:42 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -7,8 +7,10 @@ import spark.template.thymeleaf.ThymeleafTemplateEngine;
 import tikape.runko.database.Database;
 import tikape.runko.database.DrinkkiDao;
 import tikape.runko.database.KategoriaDao;
+import tikape.runko.database.RaakaAineDao;
 import tikape.runko.domain.Drinkki;
 import tikape.runko.domain.Kategoria;
+import tikape.runko.domain.RaakaAine;
 
 public class Main {
 
@@ -23,17 +25,23 @@ public class Main {
 
         DrinkkiDao drinkkiDao = new DrinkkiDao(database);
         KategoriaDao kategoriaDao = new KategoriaDao(database);
+        RaakaAineDao raakaAineDao = new RaakaAineDao(database);
 
         get("/", (req, res) -> {
             HashMap map = new HashMap<>();
-            map.put("viesti", "tervehdys");
-
             return new ModelAndView(map, "index");
         }, new ThymeleafTemplateEngine());
 
         get("/drinkit", (req, res) -> {
             HashMap map = new HashMap<>();
             map.put("drinkit", drinkkiDao.findAll());
+
+            return new ModelAndView(map, "drinkit");
+        }, new ThymeleafTemplateEngine());
+        
+        get("/drinkit/kategoriat/:kategoria", (req, res) -> {
+            HashMap map = new HashMap<>();
+            map.put("drinkit", drinkkiDao.findAllbyCategory(req.params("kategoria")));
 
             return new ModelAndView(map, "drinkit");
         }, new ThymeleafTemplateEngine());
@@ -58,6 +66,22 @@ public class Main {
             String kategoria = req.queryParams("kategoria");
             kategoriaDao.lisaaDrinkki(kategoriaDao.findOnebyName(kategoria), drinkkiDao.findOnebyName(nimi) );
             res.redirect("/");
+            return "";
+        });
+        
+        get("/raaka-aineet", (req, res) -> {
+            HashMap map = new HashMap<>();
+            map.put("raakaAineet", raakaAineDao.findAll());
+            map.put("tilasto", raakaAineDao.findStatistics());
+
+            return new ModelAndView(map, "raaka-aineet");
+        }, new ThymeleafTemplateEngine());
+        
+        post("/raaka-aineet", (req, res) -> {
+            String nimi = req.queryParams("nimi");
+            RaakaAine raakaAine = new RaakaAine(0, nimi);
+            raakaAineDao.save(raakaAine);
+            res.redirect("/raaka-aineet");
             return "";
         });
     }
